91600f601a1716e47a243277a97c31f64196a79b
Your Name <you@example.com>
2020-01-18 02:01:47 +0545

    changed

--- lib/Inside app/profile.dart
+++ lib/Inside app/profile.dart
@@ -1,51 +1,310 @@
 import 'package:flutter/material.dart';
+import'dart:math';
 
 class Profile extends StatelessWidget {
+  final List <int> a = [10, 20, 30, 40];
+  final List<String> foodImages = [
+    "assets/burger.jpg",
+    "assets/momo.jpg",
+    "assets/pizza.jpg",
+    "assets/chowmin.jpg",
+  ];
+  static var random = Random.secure();
+
+  var index = random.nextInt(4);
+  String userName="UserName";
+  String phoNo ="+97798XXXXXXXX";
+  int visitedRestaurant = 23;
+  int waiting = 4;
+  int bonusPoint = 100;
+
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      body:
-          Column(
-          children : <Widget>[
-      Container(
-        height: double.infinity,
-        child: SingleChildScrollView(
-          physics: AlwaysScrollableScrollPhysics(),
-          padding: EdgeInsets.symmetric(
-            horizontal: 20.0,
-            vertical: 80.0,
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-          Text(
-          'Hi! Aaditya Subedi , Good Morning',
-            style:TextStyle(
-              color: Colors.red,
-              letterSpacing: 1.5,
-              fontSize:20.0,
-              fontWeight: FontWeight.bold,
-              fontFamily: 'OpenSans',
+    return Scaffold(
+//        backgroundColor: const Color(0xFFC89309),
+      body: Stack(
+          children: <Widget>[
+            SizedBox.expand(
+              child: Image.asset(foodImages[index], fit: BoxFit.cover,),
+            ),
+            DraggableScrollableSheet(
+                minChildSize: 0.1,
+                initialChildSize: 0.22,
+                builder: (context, scrollController) {
+                  return SingleChildScrollView(
+                      controller: scrollController,
+                      child: Container(
+                          constraints: BoxConstraints(minHeight: MediaQuery
+                              .of(context)
+                              .size
+                              .height),
+                          color: Colors.white,
+                          child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: <Widget>[
+
+                                Container(
+
+                                  padding: EdgeInsets.only(
+                                      left: 32, right: 32, top: 32),
+                                  child: Row(
+                                      mainAxisAlignment: MainAxisAlignment
+                                          .spaceBetween,
+                                      children: <Widget>[
+                                        SizedBox(
+                                            height: 100,
+                                            width: 100,
+                                            child: ClipOval(
+                                              child: Image.asset(
+                                                'assets/google.png',
+                                                fit: BoxFit.cover,),
+                                            )
+                                        ),
+                                        SizedBox(width: 16,),
+                                        Expanded(
+                                            child: Column(
+                                                crossAxisAlignment: CrossAxisAlignment
+                                                    .start,
+                                                children: <Widget>[
+                                                  Text(userName,
+                                                    style: TextStyle(
+                                                        color: Colors
+                                                            .grey[800],
+                                                        fontSize: 36,
+                                                        fontWeight: FontWeight
+                                                            .w700),),
+
+                                                  Text(phoNo,
+                                                    style: TextStyle(
+                                                        color: Colors
+                                                            .grey[800],
+                                                        fontSize: 16,
+                                                        fontWeight: FontWeight
+                                                            .w400),),
 
 
-                ),
-          ),
-              SizedBox(height: 30),
-            Column(
-             children:<Widget> [
-               Image.asset("assets/Profile.jpg",
-                 height: 100,
-            width : 100,
-          ),
-             ]
-          ),
-        ],
-          ),
-        )
-          ),
-    ]
-    )
+                                                ]
+                                            )
+                                        ),
+
+
+                                      ]
+                                  ),
+                                ),
+                                SizedBox(height: 16,),
+                                Container(
+                                  padding: EdgeInsets.all(32),
+                                  color: Colors.blue,
+                                  child: Row(
+                                      mainAxisAlignment: MainAxisAlignment
+                                          .spaceBetween,
+                                      children: <Widget>[
+                                        Column(
+                                            children: <Widget>[
+                                              Row(
+                                                  crossAxisAlignment: CrossAxisAlignment
+                                                      .center,
+                                                  children: <Widget>[
+                                                    Icon(Icons.check_box,
+                                                        color: Colors.white,
+                                                        size: 30),
+                                                    SizedBox(width: 4),
+                                                    Text(visitedRestaurant.toString(), style:
+                                                    TextStyle(
+                                                      color: Colors.white,
+                                                      fontWeight: FontWeight
+                                                          .w700,
+                                                      fontSize: 24,
+                                                    )
+                                                    )
+                                                  ]
+
+                                              ),
+                                              Text(
+                                                  "VIsited Restaurant", style:
+                                              TextStyle(
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.w700,
+                                                fontSize: 15,
+
+
+                                              )
+                                              )
+                                            ]
+
+                                        ),
+
+                                        Column(
+                                            children: <Widget>[
+                                              Row(
+                                                  crossAxisAlignment: CrossAxisAlignment
+                                                      .center,
+                                                  children: <Widget>[
+                                                    Icon(Icons.access_alarm,
+                                                        color: Colors.white,
+                                                        size: 30),
+                                                    SizedBox(width: 4),
+                                                    Text(waiting.toString(), style:
+                                                    TextStyle(
+                                                      color: Colors.white,
+                                                      fontWeight: FontWeight
+                                                          .w700,
+                                                      fontSize: 24,
+                                                    )
+                                                    )
+                                                  ]
+
+                                              ),
+                                              Text("Waiting", style:
+                                              TextStyle(
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.w700,
+                                                fontSize: 15,
+
+
+                                              )
+                                              )
+                                            ]
+
+                                        ),
+
+                                        Column(
+                                            children: <Widget>[
+                                              Row(
+                                                  crossAxisAlignment: CrossAxisAlignment
+                                                      .center,
+                                                  children: <Widget>[
+
+                                                    SizedBox(width: 4),
+                                                    Text(("Rs."+ bonusPoint.toString()), style:
+                                                    TextStyle(
+                                                      color: Colors.white,
+                                                      fontWeight: FontWeight
+                                                          .w700,
+                                                      fontSize: 24,
+                                                    )
+                                                    )
+                                                  ]
+
+                                              ),
+                                              Text("Bonus point", style:
+                                              TextStyle(
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.w700,
+                                                fontSize: 15,
+
+
+                                              )
+                                              )
+                                            ]
+
+                                        )
+
+
+                                      ]
+                                  ),
+
+                                ),
+                                SizedBox(height: 16,),
+                                Container(
+                                    padding: EdgeInsets.all(32),
+                                    color: Colors.brown,
+                                    child: Column(
+                                        children: <Widget>[
+                                          SizedBox(height:25),
+                                          Row(children: <Widget>
+                                          [
+                                            Icon(Icons.,
+                                                color: Colors.black,
+                                                size: 35),
+                                            SizedBox(width: 20),
+                                            Text("Invite & earn", style:
+                                            TextStyle(
+                                              color: Colors.black,
+                                              fontWeight: FontWeight.w700,
+                                              fontSize: 30,
+
+                                            ))
+                                          ]),
+                                          Row(children: <Widget>
+                                          [
+                                            Icon(Icons.settings,
+                                                color: Colors.black,
+                                                size: 35),
+                                            SizedBox(width: 20),
+                                            Text("Settings", style:
+                                            TextStyle(
+                                              color: Colors.black,
+                                              fontWeight: FontWeight.w700,
+                                              fontSize: 30,
+
+                                            ))
+                                          ]),
+                                          SizedBox(height:25),
+                                          Row(children: <Widget>
+                                          [
+                                            Icon(Icons.help,
+                                                color: Colors.black,
+                                                size: 35),
+                                            SizedBox(width: 20),
+                                            Text("Help & Support", style:
+                                            TextStyle(
+                                              color: Colors.black,
+                                              fontWeight: FontWeight.w700,
+                                              fontSize: 30,
+
+                                            ))
+                                          ]),
+                                          SizedBox(height:25),
+                                          Row(children: <Widget>
+                                          [
+                                            Icon(Icons.info,
+                                                color: Colors.black,
+                                                size: 35),
+                                            SizedBox(width: 20),
+                                            Text("About", style:
+                                            TextStyle(
+                                              color: Colors.black,
+                                              fontWeight: FontWeight.w700,
+                                              fontSize: 30,
+
+                                            ))
+                                          ]),
+
+                                          SizedBox(height:25),
+                                          Row(children: <Widget>
+                                          [
+                                            Icon(Icons.exit_to_app,
+                                                color: Colors.black,
+                                                size: 35),
+                                            SizedBox(width: 20),
+                                            Text("Log out", style:
+                                            TextStyle(
+                                              color: Colors.black,
+                                              fontWeight: FontWeight.w700,
+                                              fontSize: 30,
+
+                                            ))
+                                          ]),
+                                          SizedBox(height:25),
+
+
+
+
+                                        ]
+                                    )
+                                )
+
+                              ]
+                          )
+
+                      ));
+                })
+          ]
+      ),
+
     );
   }
 }
