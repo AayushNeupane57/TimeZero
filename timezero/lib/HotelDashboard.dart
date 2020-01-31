50f69f3a357567bdebc9c45932585c3b0cf999c9
Aayush_Neupane <perfectweaponayous@gmail.com>
2020-01-31 12:53:51 +0545

    slight modification in hotel side UI hotelDashboard

--- timezero/lib/HotelDashboard.dart
+++ timezero/lib/HotelDashboard.dart
@@ -17,7 +17,7 @@ class _HotelDashboard extends State<HotelDashboard> {
   Material myItems(IconData icon, String heading, int color) {
     return Material(
         color: Colors.white,
-        elevation: 14.0,
+        elevation: 4.0,
         shadowColor: Color(0x802196F3),
         borderRadius: BorderRadius.circular(24.0),
         child: Center(
@@ -77,57 +77,60 @@ class _HotelDashboard extends State<HotelDashboard> {
           backgroundColor: Colors.redAccent,
           centerTitle: true,
           title: const Text('HotelDashbard'),),
-        body: StaggeredGridView.count(
-          crossAxisCount: 2,
-          crossAxisSpacing: 12.0,
-          mainAxisSpacing: 12.0,
-          padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-          children: <Widget>[
+        body: Container(
+          padding: EdgeInsets.symmetric(vertical: 20),
+          child: StaggeredGridView.count(
+            crossAxisCount: 2,
+            crossAxisSpacing: 8.0,
+            mainAxisSpacing: 12.0,
+            padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+            children: <Widget>[
 
-            GestureDetector(
-                onTap: () {
-                  print("Today's Business");
-                },
-                child: myItems(Icons.supervised_user_circle, "Today's business",
-                    0xff3999fe)),
+              GestureDetector(
+                  onTap: () {
+                    print("Today's Business");
+                  },
+                  child: myItems(Icons.supervised_user_circle, "Today's business",
+                      0xff3999fe)),
 
-            GestureDetector(
-                onTap: () {
-                  print("Notification");
-                },
-                child: myItems(Icons.notifications_active, "Notifications", 0xffff3266)),
-            GestureDetector(
-                onTap: () {
-                  print("Food request");
-                },
-                child: myItems(Icons.fastfood, "Food Request", 0xffffa500)),
-            GestureDetector(
-                onTap: () {
-                  print("Menu list");
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => MenuList()),
-                  );
-                },
-                child: myItems(Icons.restaurant_menu, "Menu List", 0xff1f9aa1)),
-            GestureDetector(
-                onTap: () {
-                  print("My profile");
-                },
-                child: myItems(Icons.person, "My Profile", 0xfff52abb)),
+              GestureDetector(
+                  onTap: () {
+                    print("Notification");
+                  },
+                  child: myItems(Icons.notifications_active, "Notifications", 0xffff3266)),
+              GestureDetector(
+                  onTap: () {
+                    print("Food request");
+                  },
+                  child: myItems(Icons.fastfood, "Food Request", 0xffffa500)),
+              GestureDetector(
+                  onTap: () {
+                    print("Menu list");
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => MenuList()),
+                    );
+                  },
+                  child: myItems(Icons.restaurant_menu, "Menu List", 0xff1f9aa1)),
+              GestureDetector(
+                  onTap: () {
+                    print("My profile");
+                  },
+                  child: myItems(Icons.person, "My Profile", 0xfff52abb)),
 //        MyItems(Icons.notifications_active, "Notifications", 0xffff3266),
 
 
-          ],
+            ],
 
-          staggeredTiles: [
-            StaggeredTile.extent(2, 130),
-            StaggeredTile.extent(1, 150),
-            StaggeredTile.extent(1, 150),
-            StaggeredTile.extent(1, 150),
-            StaggeredTile.extent(1, 150),
-          ],
+            staggeredTiles: [
+              StaggeredTile.extent(3, 190),
+              StaggeredTile.extent(1, 190),
+              StaggeredTile.extent(1, 190),
+              StaggeredTile.extent(1, 190),
+              StaggeredTile.extent(1, 190),
+            ],
 
+          ),
         )
     );
   }
--- timezero/lib/menulist.dart
+++ timezero/lib/menulist.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import "package:flutter/material.dart";
 import 'constants.dart';
 import'database.dart';
@@ -28,134 +29,138 @@ class MenuList extends StatefulWidget {
             centerTitle: true,
             title: const Text('Menu List'),),
           body:
-          Column(
-            children: <Widget>[
-              RaisedButton(
-                onPressed: () {
-                  showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return AlertDialog(
-                          content: Form(
-                            key: _formKey,
-                            child: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children: <Widget>[
-                                Padding(
-                                  padding: EdgeInsets.all(8.0),
-                                  child: TextFormField(
-                                    decoration: const InputDecoration(
-                                      icon: Icon(Icons.text_fields),
-                                      hintText: 'Enter the name of the food item',
-                                      labelText: 'Food Name',
-                                    ),
-                                    onSaved: (String value) {
-                                     print(value);
-                                     nameOfFood = value;
-                                    },
-                                  )
-                                ),
-                                Padding(
-                                  padding: EdgeInsets.all(8.0),
-                                  child:TextFormField(
-                                    decoration: const InputDecoration(
-                                      icon: Icon(Icons.text_fields),
-                                      hintText: 'Enter the rate',
-                                      labelText: 'Rate',
-                                    ),
-                                    onSaved: (String value) {
-
-                                      rate = double.parse(value) ;
-                                      print(rate);
-                                    },
-                                  ),
-                                ),
-
-                                Padding(
-                                  padding: EdgeInsets.all(8.0),
-                                  child:TextFormField(
-                                    decoration: const InputDecoration(
-                                      icon: Icon(Icons.text_fields),
-                                      hintText: 'choose photo',
-                                      labelText: 'Photo',
-                                    ),
-                                    onSaved: (String value) {
-
-                                      imageAddress = value;
-                                      print(imageAddress);
-                                    },
-                                  ),
-                                ),
-
-
-
-
-
-
-                                Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: RaisedButton(
-                                    child: Text("Submit"),
-                                    onPressed: () {
-                                      if (_formKey.currentState.validate()) {
-                                        _formKey.currentState.save();
-                                        setState(() {
-                                          foodDetail.add( FoodDetail(nameOfFood ,"Chow%20Mein.jpg?alt=media&token=242660c5-1369-4ad5-8077-eaba5835c66b",rate));
-                                          Navigator.of(context, rootNavigator: true).pop('dialog');
-                                        });
-                                      }
-                                    },
-                                  ),
-                                ),
-
-
-                              ],
-                            ),
-                          ),
-                        );
-                      });
-                },
-                child: Text("Add on menu list",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 15
-                )),
-              ),
-              SizedBox(height: 20),
-
-              Expanded(
-                child: ListView.builder(
-                    itemCount: foodDetail.length,
-                    scrollDirection: Axis.vertical,
-                    itemBuilder: (context, index) {
-                      return Dismissible(
-                          key: Key(foodDetail[index].name),
-                          onDismissed: (direction) {
-                            setState(() {
-                              foodDetail.removeAt(index);
-                            });
-                          },
-                          child: Row(
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              children: <Widget>[
+                RaisedButton(
+                  onPressed: () {
+                    showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            content: Form(
+                              key: _formKey,
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
                                 children: <Widget>[
-                                  Text('${index+1}.',style:kMenuStyle),
-                                  Spacer(),
-                                  CircleAvatar(
-                                backgroundImage: NetworkImage("https://firebasestorage.googleapis.com/v0/b/timezero-2c099.appspot.com/o/images%2F${foodDetail[index].image}"),
-                                minRadius: 20,
-                                maxRadius: 20,
+                                  Padding(
+                                    padding: EdgeInsets.all(8.0),
+                                    child: TextFormField(
+                                      decoration: const InputDecoration(
+                                        icon: Icon(Icons.text_fields),
+                                        hintText: 'Enter the name of the food item',
+                                        labelText: 'Food Name',
+                                      ),
+                                      onSaved: (String value) {
+                                       print(value);
+                                       nameOfFood = value;
+                                      },
+                                    )
                                   ),
-                                  Spacer(),
-                                  Text(foodDetail[index].name,style:kMenuStyle),
+                                  Padding(
+                                    padding: EdgeInsets.all(8.0),
+                                    child:TextFormField(
+                                      decoration: const InputDecoration(
+                                        icon: Icon(Icons.text_fields),
+                                        hintText: 'Enter the rate',
+                                        labelText: 'Rate',
+                                      ),
+                                      onSaved: (String value) {
 
-                                  Spacer(),
-                                  Text("Rs. ${foodDetail[index].rate}",style:kMenuStyle),
+                                        rate = double.parse(value) ;
+                                        print(rate);
+                                      },
+                                    ),
+                                  ),
+
+                                  Padding(
+                                    padding: EdgeInsets.all(8.0),
+                                    child:TextFormField(
+                                      decoration: const InputDecoration(
+                                        icon: Icon(Icons.text_fields),
+                                        hintText: 'choose photo',
+                                        labelText: 'Photo',
+                                      ),
+                                      onSaved: (String value) {
+
+                                        imageAddress = value;
+                                        print(imageAddress);
+                                      },
+                                    ),
+                                  ),
+
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: RaisedButton(
+                                      child: Text("Submit"),
+                                      onPressed: () {
+                                        if (_formKey.currentState.validate()) {
+                                          _formKey.currentState.save();
+                                          setState(() {
+                                            foodDetail.add( FoodDetail(nameOfFood ,"Chow%20Mein.jpg?alt=media&token=242660c5-1369-4ad5-8077-eaba5835c66b",rate));
+                                            Navigator.of(context, rootNavigator: true).pop('dialog');
+                                          });
+                                        }
+                                      },
+                                    ),
+                                  ),
+
 
                                 ],
-                              ));
-                    }),
-              )
+                              ),
+                            ),
+                          );
+                        });
+                  },
+                  child: Text("Add on menu list",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15
+                  )),
+                ),
+                SizedBox(height: 20),
 
-            ],
+                Expanded(
+                  child: ListView.builder(
+                      itemCount: foodDetail.length,
+                      scrollDirection: Axis.vertical,
+                      itemBuilder: (context, index) {
+                        return Dismissible(
+                            key: Key(foodDetail[index].name),
+                            onDismissed: (direction) {
+                              setState(() {
+                                foodDetail.removeAt(index);
+                              });
+                            },
+                            child: Row(
+                                  children: <Widget>[
+                                    Expanded(
+                                      flex:1,
+                                        child: Text('${index+1}.',style:kMenuStyle)),
+                                    Expanded(
+                                      flex:2,
+                                      child: CircleAvatar(
+                                  backgroundImage: NetworkImage("https://firebasestorage.googleapis.com/v0/b/timezero-2c099.appspot.com/o/images%2F${foodDetail[index].image}"),
+                                  minRadius: 20,
+                                  maxRadius: 20,
+                                      ),
+                                    ),
+                                    Expanded(
+                                        flex:3,
+                                        child: Text(foodDetail[index].name,style:kMenuStyle)),
+
+                                    Expanded(
+                                        flex:2,
+                                        child: Text("Rs. ${foodDetail[index].rate}",style:kMenuStyle)),
+
+                                  ],
+                                ));
+                      }),
+                )
+
+              ],
+            ),
           ),
         );
     }
