4418a223a8e48650cbf85127e9cb2f604d43fd73
Your Name <you@example.com>
2020-01-31 23:21:15 +0545

    menulist

--- timezero/lib/menulist.dart
+++ timezero/lib/menulist.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import "package:flutter/material.dart";
+import 'package:timezero/data.dart';
 import 'constants.dart';
 import'database.dart';
 import 'dart:io';
 import 'package:image_picker/image_picker.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 
 class MenuList extends StatefulWidget {
@@ -20,155 +22,183 @@ class MenuList extends StatefulWidget {
     String imageAddress = "";
     File _image;
 
+
     final _formKey = GlobalKey<FormState>();
 
     @override
     Widget build(BuildContext context) {
-      return
-        Scaffold(
+      return StreamBuilder(
+          stream: Firestore.instance.collection("Soltae").snapshots(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+              case ConnectionState.waiting:
+                return Center(
+                    child: CircularProgressIndicator());
+              default:
+                return
+                  Scaffold(
+                    backgroundColor: Colors.grey[100],
+                    appBar: AppBar(
+                      backgroundColor: Colors.redAccent,
+                      centerTitle: true,
+                      title: const Text('Menu List'),),
+                    body: Container(
+                      padding: EdgeInsets.all(20),
+                      child: Column(
+                        children: <Widget>[
+                          RaisedButton(
+                            onPressed: () {
+                              showDialog(
+                                  context: context,
+                                  builder: (BuildContext context) {
+                                    return AlertDialog(
+                                      content: Form(
+                                        key: _formKey,
+                                        child: Column(
+                                          mainAxisSize: MainAxisSize.min,
+                                          children: <Widget>[
+                                            Padding(
+                                                padding: EdgeInsets.all(8.0),
+                                                child: TextFormField(
+                                                  decoration: const InputDecoration(
+                                                    icon: Icon(
+                                                        Icons.text_fields),
+                                                    hintText: 'Enter the name of the food item',
+                                                    labelText: 'Food Name',
+                                                  ),
+                                                  onSaved: (String value) {
+                                                    print(value);
+                                                    nameOfFood = value;
+                                                  },
+                                                )
+                                            ),
+                                            Padding(
+                                              padding: EdgeInsets.all(8.0),
+                                              child: TextFormField(
+                                                decoration: const InputDecoration(
+                                                  icon: Icon(Icons.text_fields),
+                                                  hintText: 'Enter the rate',
+                                                  labelText: 'Rate',
+                                                ),
+                                                onSaved: (String value) {
+                                                  rate = value;
+                                                  print(rate);
+                                                },
+                                              ),
+                                            ),
 
-          backgroundColor: Colors.grey[100],
-          appBar: AppBar(
-            backgroundColor: Colors.redAccent,
-            centerTitle: true,
-            title: const Text('Menu List'),),
-          body: Container(
-            padding: EdgeInsets.all(20),
-            child: Column(
-              children: <Widget>[
-                RaisedButton(
-                  onPressed: () {
-                    showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return AlertDialog(
-                            content: Form(
-                              key: _formKey,
-                              child: Column(
-                                mainAxisSize: MainAxisSize.min,
-                                children: <Widget>[
-                                  Padding(
-                                    padding: EdgeInsets.all(8.0),
-                                    child: TextFormField(
-                                      decoration: const InputDecoration(
-                                        icon: Icon(Icons.text_fields),
-                                        hintText: 'Enter the name of the food item',
-                                        labelText: 'Food Name',
-                                      ),
-                                      onSaved: (String value) {
-                                        print(value);
-                                       nameOfFood = value;
-                                      },
-                                    )
-                                  ),
-                                  Padding(
-                                    padding: EdgeInsets.all(8.0),
-                                    child:TextFormField(
-                                      decoration: const InputDecoration(
-                                        icon: Icon(Icons.text_fields),
-                                        hintText: 'Enter the rate',
-                                        labelText: 'Rate',
-                                      ),
-                                      onSaved: (String value) {
+                                            Padding(
+                                              padding: EdgeInsets.all(8.0),
+                                              child: TextFormField(
+                                                decoration: const InputDecoration(
+                                                  icon: Icon(Icons.text_fields),
+                                                  hintText: 'choose photo',
+                                                  labelText: 'Photo',
+                                                ),
+                                                onSaved: (String value) {
+                                                  imageAddress = value;
+                                                  print(imageAddress);
+                                                },
+                                              ),
+                                            ),
 
-                                        rate = value ;
-                                        print(rate);
-                                      },
-                                    ),
-                                  ),
-
-                                  Padding(
-                                    padding: EdgeInsets.all(8.0),
-                                    child:TextFormField(
-                                      decoration: const InputDecoration(
-                                        icon: Icon(Icons.text_fields),
-                                        hintText: 'choose photo',
-                                        labelText: 'Photo',
-                                      ),
-                                      onSaved: (String value) {
-
-                                        imageAddress = value;
-                                        print(imageAddress);
-                                      },
-                                    ),
-                                  ),
-
-                                  Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: RaisedButton(
-                                      child: Text("Submit"),
-                                      onPressed: () {
-                                        if (_formKey.currentState.validate()) {
-                                          _formKey.currentState.save();
-                                          setState(() {
+                                            Padding(
+                                              padding: const EdgeInsets.all(
+                                                  8.0),
+                                              child: RaisedButton(
+                                                child: Text("Submit"),
+                                                onPressed: () {
+                                                  if (_formKey.currentState
+                                                      .validate()) {
+                                                    _formKey.currentState
+                                                        .save();
+                                                    setState(() {
+                                                      Data data = new Data();
+                                                      data.setFoodItems(
+                                                          "Soltae", nameOfFood,
+                                                          "Chow%20Mein.jpg?alt=media&token=242660c5-1369-4ad5-8077-eaba5835c66b",
+                                                          "500000");
 //                                            foodDetail.add( Items(itemName:nameOfFood ,itemImage:"Chow%20Mein.jpg?alt=media&token=242660c5-1369-4ad5-8077-eaba5835c66b",itemPrice: rate));
-                                            foodsSoltae.add( Items(itemName:nameOfFood ,itemImage: "Chow%20Mein.jpg?alt=media&token=242660c5-1369-4ad5-8077-eaba5835c66b", itemPrice: rate,associatedHotel:"Soltae"));
+//                                            foodsSoltae.add( Items(itemName:nameOfFood ,itemImage: "Chow%20Mein.jpg?alt=media&token=242660c5-1369-4ad5-8077-eaba5835c66b", itemPrice: rate,associatedHotel:"Soltae"));
 
-                                            Navigator.of(context, rootNavigator: true).pop('dialog');
-                                          });
-                                        }
-                                      },
-                                    ),
-                                  ),
+                                                      Navigator.of(context,
+                                                          rootNavigator: true)
+                                                          .pop(
+                                                          'dialog');
+                                                    });
+                                                  }
+                                                },
+                                              ),
+                                            ),
 
 
-                                ],
-                              ),
-                            ),
-
-                          );
-                        });
-                  },
-                  child: Text("Add on menu list",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 15
-                  )),
-                ),
-                SizedBox(height: 20),
-
-                Expanded(
-                  child: ListView.builder(
-                      itemCount: foodsSoltae.length,
-                      scrollDirection: Axis.vertical,
-                      itemBuilder: (context, index) {
-                        return Dismissible(
-                            key: Key(foodsSoltae[index].itemName),
-                            onDismissed: (direction) {
-                              setState(() {
-                                foodsSoltae.removeAt(index);
-                              });
-                            },
-                            child: Row(
-                                  children: <Widget>[
-                                    Expanded(
-                                      flex:1,
-                                        child: Text('${index+1}.',style:kMenuStyle)),
-                                    Expanded(
-                                      flex:2,
-                                      child: CircleAvatar(
-//                                  backgroundImage: NetworkImage("https://firebasestorage.googleapis.com/v0/b/timezero-2c099.appspot.com/o/images%2F${foodDetail[index].image}"),
-                                  minRadius: 20,
-                                  maxRadius: 20,
+                                          ],
+                                        ),
                                       ),
-                                    ),
-                                    Expanded(
-                                        flex:3,
-                                        child: Text(foodsSoltae[index].itemName,style:kMenuStyle)),
 
-                                    Expanded(
-                                        flex:2,
-                                        child: Text("Rs. ${foodsSoltae[index].itemPrice}",style:kMenuStyle)),
+                                    );
+                                  });
+                            },
+                            child: Text("Add on menu list",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 15
+                                )),
+                          ),
+                          SizedBox(height: 20),
 
-                                  ],
-                                ));
-                      }),
-                )
+                          Expanded(
+                            child: ListView.builder(
+                                itemCount: snapshot.data.documents.length,
+                                scrollDirection: Axis.vertical,
+                                itemBuilder: (context, index) {
+                                  return Dismissible(
+                                      key: Key(snapshot.data.documents[index].documentID),
+                                      onDismissed: (direction) {
+                                        setState(() {
+                                          Firestore.instance.collection("Soltae").document(snapshot.data.documents[index].documentID).delete();
+                                        });
+                                      },
+                                      child: Row(
+                                        children: <Widget>[
+                                          Expanded(
+                                              flex: 1,
+                                              child: Text('${index + 1}.',
+                                                  style: kMenuStyle)),
+                                          Expanded(
+                                            flex: 2,
+                                            child: CircleAvatar(
+//                                  backgroundImage: NetworkImage("https://firebasestorage.googleapis.com/v0/b/timezero-2c099.appspot.com/o/images%2F${foodDetail[index].image}"),
+                                              minRadius: 20,
+                                              maxRadius: 20,
+                                            ),
+                                          ),
+                                          Expanded(
+                                              flex: 3,
+                                              child: Text(
+                                                  snapshot.data.documents[index].documentID,
+                                                  style: kMenuStyle)),
 
-              ],
-            ),
-          ),
-        );
+                                          Expanded(
+                                              flex: 2,
+                                              child: Text(
+
+                                                  "Rs. ${snapshot.data.documents[index]['price']}",
+                                                  style: kMenuStyle)),
+
+                                        ],
+                                      ));
+                                }),
+                          )
+
+                        ],
+                      ),
+                    ),
+                  );
+            }
+          }
+      );
     }
 
 
