dc861501457434bd2839fd8cf730718f0a176fab
Your Name <you@example.com>
2020-02-01 08:50:25 +0545

    d

--- timezero/lib/foodRequests.dart
+++ timezero/lib/foodRequests.dart
@@ -8,8 +8,8 @@ class FoodRequest extends StatefulWidget {
 }
 
 class _FoodRequestState extends State<FoodRequest> {
-  String icon;
-  String text;
+  var icon = Icons.help;
+  String acceptText ="Accept Request ?";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,99 +19,125 @@ class _FoodRequestState extends State<FoodRequest> {
             stream: Firestore.instance.collection('BookedDetails').snapshots(),
             builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
               if (!snapshot.hasData) return new Text('Loading...');
-              return Container(
-                child: new ListView(
-                  children: snapshot.data.documents.map((DocumentSnapshot document) {
-                    return new ListTile(
-                      title: new Text(document.data["itemName"].toString()),
-                      subtitle: Container(
+              return
+                ListView.builder(
+                    itemCount: snapshot.data.documents.length,
+                    scrollDirection: Axis.vertical,
+                    itemBuilder: (context, index) {
+                      return new ListTile(
+                        title: new Text(snapshot.data
+                            .documents[index]["itemName"].toString()),
+                        subtitle: Container(
 
-                        margin: EdgeInsets.all(10),
-                        width: MediaQuery.of(context).size.width,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                            boxShadow: [
-              BoxShadow(
-                blurRadius: 5,
-                offset: Offset(0, 3),
-                color: Colors.grey,
-              )]
-              ),
-                        padding: EdgeInsets.all(15.0),
-                        child: Column(
-                          children: <Widget>[
-                            Text("ordered Time : "+DateFormat.yMd().add_jm().format(document.data["orderedTime"].toDate().toLocal())),
-                            SizedBox(width: 30,),
-                            Text("arrival  : "+DateFormat.yMd().add_jm().format(document.data["comingTime"].toDate().toLocal())),
-                            Text(document.data["userName"].toString()),
-                            Text(document.data["email"].toString()),
-                            // Text("food item : " + document.data["itemName"].toString()),
+                          margin: EdgeInsets.all(10),
+                          width: MediaQuery
+                              .of(context)
+                              .size
+                              .width,
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              boxShadow: [
+                                BoxShadow(
+                                  blurRadius: 5,
+                                  offset: Offset(0, 3),
+                                  color: Colors.grey,
+                                )
+                              ]
+                          ),
+                          padding: EdgeInsets.all(15.0),
+                          child: Column(
+                            children: <Widget>[
+                              Text("ordered Time : " +
+                                  DateFormat.yMd().add_jm().format(snapshot.data
+                                      .documents[index]["orderedTime"]
+                                      .toDate()
+                                      .toLocal())),
+                              SizedBox(width: 30,),
+                              Text("arrival  : " +
+                                  DateFormat.yMd().add_jm().format(snapshot.data
+                                      .documents[index]["comingTime"]
+                                      .toDate()
+                                      .toLocal())),
+                              Text(snapshot.data.documents[index]["userName"]
+                                  .toString()),
+                              Text(snapshot.data.documents[index]["email"]
+                                  .toString()),
+                              // Text("food item : " + document.data["itemName"].toString()),
 
-                            Row(
-                              children: <Widget>[
-                                Column(
-                                    children:<Widget>[
-                                      IconButton(
-                                        icon: Icon(Icons.help,
-                                          color: Colors.green,),
-                                        onPressed: () {
-
-
-                                        },
-                                      ),
-                                      GestureDetector(
-                                          child:
-                                          Text("Accept Request",
-                                              style: TextStyle(
-                                                  fontSize: 14)),
-                                          onTap:(){
-                                            print("Accept button  pressed");
-
-                                          }
-                                      ),
-                                    ]
-                                ),
-
-
-
-                                Spacer(),
-
-                                Column(
-                                    children:<Widget>[
-                                      IconButton(
-                                        icon: Icon(Icons.cancel,
-                                            color:Colors.red),
-                                        onPressed: () {
-                                          setState(() {
-
-                                          });
-                                          print("Cancel button  pressed");
-                                        },
-                                      ),
-                                      Text("Cancel Request",
-                                          style: TextStyle(
-                                              fontSize: 14)),
-
-                                    ]
-                                ),
+                              Row(
+                                children: <Widget>[
+                                  Column(
+                                      children: <Widget>[
+                                        IconButton(
+                                          icon: Icon(icon,
+                                            color: Colors.green,),
+                                          onPressed: () {
+                                            setState(() {
+                                              icon = Icons.check_box;
+                                              acceptText ="Accepted";
+                                            });
 
 
-                              ],
-                            ),
 
 
-                          ],
+                                          },
+                                        ),
+                                        GestureDetector(
+                                            child:
+                                            Text(acceptText,
+                                                style: TextStyle(
+                                                    fontSize: 14)),
+                                            onTap: () {
+                                              print("Accept button  pressed");
+                                            }
+                                        ),
+                                      ]
+                                  ),
+
+
+                                  Spacer(),
+
+                                  Column(
+                                      children: <Widget>[
+                                        IconButton(
+                                          icon: Icon(Icons.cancel,
+                                              color: Colors.red),
+                                          onPressed: () {
+                                            setState(() {
+                                              Firestore.instance.collection(
+                                                  "BookedDetails").document(
+                                                  snapshot.data.documents[index]
+                                                      .documentID)
+                                                  .delete();
+                                              print(index);
+                                            });
+
+                                          },
+                                        ),
+                                        Text("Cancel Request",
+                                            style: TextStyle(
+                                                fontSize: 14)),
+
+                                      ]
+                                  ),
+
+
+                                ],
+                              ),
+
+
+                            ],
+                          ),
                         ),
-                      ),
-                    );
-                  }).toList(),
-                ),
-              );
-            },
+                      );
+                    });
+            }
+            )
+
           ),
 
         ),
-      ),
-    );
+      );
+
   }
 }
